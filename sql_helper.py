92c563487a0c53d0808abf6f9d1e51710dc4a922
vmichael89 <102418027+vmichael89@users.noreply.github.com>
2023-07-23 05:15:04 +0200

    added mapping of tables through primary and foreign keys

--- sql_helper.py
+++ sql_helper.py
@@ -28,6 +28,8 @@ class Database:
         self.info = self.get_info()
         self.table_names = self.get_table_names()
         self.tables = self.get_tables()
+        self.mapping = self.get_mapping()
+        self.table_mapping = dict(self.mapping[["from_table", "to_table"]].groupby("from_table")["to_table"].apply(list))
 
     def query(self, prompt) -> pd.DataFrame:
         """
@@ -80,6 +82,49 @@ class Database:
         their content as values in dataframe format."""
 
         return {table_name: self.query(f"SELECT * FROM {table_name}") for table_name in list(self.table_names)}
+
+    def get_mapping(self) -> pd.DataFrame:
+        """
+        Retrieves a DataFrame that maps foreign key relationships between tables in a PostgreSQL database.
+
+        The DataFrame contains four columns: 'from_table', 'from_column', 'to_table', 'to_column'. Each row represents a
+        foreign key relationship, where 'from_table' and 'from_column' are the source table and column, and 'to_table'
+        and 'to_column' are the target table and column that the source references.
+
+        The function fetches relationships in both directions (from source to target and from target to source). This ensures
+        that all relationships are captured, regardless of the direction.
+
+        Only foreign key relationships within the 'public' schema are considered.
+
+        Note: The function relies on system views in the information_schema to fetch the foreign key relationships.
+
+        Returns:
+            pd.DataFrame: DataFrame mapping foreign key relationships between tables.
+        """
+
+        return self.query(
+            "SELECT \n"
+            "	A.table_name AS from_table,\n"
+            "	A.column_name AS from_column,\n"
+            "	B.table_name AS to_table,\n"
+            "	B.column_name AS to_column\n"
+            "FROM information_schema.key_column_usage A\n"
+            "INNER JOIN information_schema.constraint_column_usage B ON A.constraint_name=B.constraint_name\n"
+            "INNER JOIN information_schema.table_constraints C ON A.constraint_name=C.constraint_name\n"
+            "WHERE A.table_schema = 'public'\n"
+            "	AND C.constraint_type = 'FOREIGN KEY'\n"
+            "UNION ALL\n"
+            "SELECT \n"
+            "	B.table_name AS from_table,\n"
+            "	B.column_name AS from_column,\n"
+            "	A.table_name AS to_table,\n"
+            "	A.column_name AS to_column\n"
+            "FROM information_schema.key_column_usage A\n"
+            "INNER JOIN information_schema.constraint_column_usage B ON A.constraint_name=B.constraint_name\n"
+            "INNER JOIN information_schema.table_constraints C ON A.constraint_name=C.constraint_name\n"
+            "WHERE A.table_schema = 'public'\n"
+            "	AND C.constraint_type = 'FOREIGN KEY'\n"
+            "ORDER BY from_table, from_column\n")
 
 
 class QueryMaker:
